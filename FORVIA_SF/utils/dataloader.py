718ee0bf921f1f570acd7dc959c577a9588999dc
a01736397-coder <a01736397@tec.mx>
2025-11-19 18:05:28 -0600

    path nuevo

--- FORVIA_SF/utils/dataloader.py
+++ FORVIA_SF/utils/dataloader.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 @st.cache_data
 def load_percentage_data():
-    return pd.read_csv('FORVIA_SF/data/percentage_not_completed.csv')
+    return pd.read_csv('data/percentage_not_completed.csv')
 
 @st.cache_data
 def load_project_data():
-    df=pd.read_csv('FORVIA_SF/data/projects.csv', encoding='latin1')
+    df=pd.read_csv('data/projects.csv', encoding='latin1')
     df= df.iloc[:-2].copy()
     df['Percent complete'] = pd.to_numeric(df['Percent complete'], errors='coerce')
